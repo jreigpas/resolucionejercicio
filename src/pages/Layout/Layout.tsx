fcdeb4b448da014b56e911f7a959fe74bba80fa8
J.Javier Reig <jreigpas@nttdata.com>
2022-04-27 14:15:14 +0200

    adding switch

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,4 +1,3 @@
-//import { Outlet, Link } from "react-router-dom";
 import { Link, Outlet } from "react-router-dom";
 import AsideContent from "../Aside/AsideContent";
 import "./Layout.css";
--- src/pages/Listado/Listado.tsx
+++ src/pages/Listado/Listado.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+import ToggleSwitch from "../../components/ToggleSwitch/ToggleSwitch";
+
 const Listado = () => {
+
+  const [checked, setChecked] = useState(true);
 
   return (
     <div className="main">
      <h2>Listado</h2>
+     <ToggleSwitch id="switch" checked={checked} onChange={checked => setChecked(checked)} />
      <p>
       Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
       nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
